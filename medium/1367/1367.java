9bbd60ff590899cf2f024500cc08a123e90f7160
David Ruiz dC <drd733@alumnos.unican.es>
2024-09-07 16:56:34 +0200

    Upload Problem 1367

--- medium/1367/1367.java
+++ medium/1367/1367.java
@@ -25,7 +25,35 @@
  */
 class Solution {
     public boolean isSubPath(ListNode head, TreeNode root) {
+        if (head == null) {
+            return true;
+        }
         
+        if (root == null) {
+            return false;
+        }
+
+        if (dfs(head, root)) {
+            return true;
+        }
+
+        return isSubPath(head, root.left) || isSubPath(head, root.right);
+    }
+
+    private boolean dfs(ListNode head, TreeNode root) {
+        if (head == null) {
+            return true;
+        }
+        
+        if (root == null) {
+            return false;
+        }
+
+        if (head.val != root.val) {
+            return false;
+        }
+
+        return dfs(head.next, root.left) || dfs(head.next, root.right);
     }
 
     public class TreeNode {
